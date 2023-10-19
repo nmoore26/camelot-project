b831776cafbd30be6985ec0fd18460dbc6acd89f
Miles <Miles@10.129.169.47>
2023-10-18 19:02:05 -0500

    This is the implemented wait function

--- CamelotJava/src/com/actions/Wait.java
+++ CamelotJava/src/com/actions/Wait.java
@@ -1,5 +1,18 @@
 package com.actions;
 
-public class Wait {
+
+public class Wait implements IAction{
+	@Override
+	public String getName() {
+		return "Wait";
+	}
+	@Override
+	public boolean getShouldWait() {
+		return true;
+	}
+	private double time;
+	public Wait (double time) {
+		this.time = time;
+	}
 	
 }
