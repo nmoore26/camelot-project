41e3594bd9e9c52291dcfd6b6188bda0019f1573
Miles <Miles@10.128.167.135>
2023-10-22 10:52:08 -0500

    Sleep action again

--- CamelotJava/src/com/actions/Sleep.java
+++ CamelotJava/src/com/actions/Sleep.java
@@ -1,5 +1,22 @@
 package com.actions;
 
-public class Sleep {
+import com.entities.*;
+import com.entities.Character;
 
+public class Sleep implements IAction {
+	private Character character;
+	private Furniture furniture;
+	@Override
+	public String getName() {
+		return "Sleep";
+	}
+	@Override
+	public boolean getShouldWait() {
+		return true;
+	}
+	public Sleep(Character character, Furniture furniture ) {
+		this.character = character;
+		this.furniture = furniture;
+				
+	}
 }
